37a3b13e5b14297855156ebf79d8894eb422743d
ELEOMAR <kaotikus27@gmail.com>
2025-03-20 16:08:55 +0800

    assignment 4

--- assignments/basics-assignment-4-problem/app.js
+++ assignments/basics-assignment-4-problem/app.js
@@ -2,17 +2,17 @@ const app = Vue.createApp({
 
     data(){
         return{
-            userInputA:'',
-            userInputB:'',
-            toggleValue:true
+            inputClass:'',
+            toggleValue:true,
+            inputBackgroundColor:''
         }
     },
 
     computed:{
        paraClasses(){
         return {
-            user1: this.userInputA === 'user1',
-            user2: this.userInputA === 'user2',
+            user1: this.inputClass === 'user1',
+            user2: this.inputClass === 'user2',
             visbile:this.toggleValue,
             hidden:!this.toggleValue
         }
