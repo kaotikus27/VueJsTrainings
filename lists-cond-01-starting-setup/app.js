63cf2e4e8019b454c21097534df9939c10c4f6bc
ELEOMAR <kaotikus27@gmail.com>
2025-03-21 11:56:34 +0800

    adding and removing

--- lists-cond-01-starting-setup/app.js
+++ lists-cond-01-starting-setup/app.js
@@ -10,6 +10,9 @@ const app = Vue.createApp({
   methods:{
     addGoal(){
       this.goals.push(this.enteredGoalValue);
+    },
+    removeGoal(index){
+      this.goals.splice(index,1)
     }
   }
 });
